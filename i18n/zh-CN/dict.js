259e240f1cd0b359a7384a5a2b3eff84fffb5320
whjin <1414848321@qq.com>
2018-11-13 17:16:52 +0800

    push code

---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,47 @@
+module.exports = {
+    experience_type: {
+        "internship": ""
+    },
+    skill_type: {
+        "frontend": "Web 前端",
+        "backend": "Web 后端",
+        "practice": "Web 实践",
+        "miscellaneous": "Web 工具",
+        "reading": "Web 阅读"
+    },
+    education_type: {
+        "undergraduate": "（本科）"
+    },
+    link_type: {
+        "homepage": "博客",
+        "segmentfault": "思否",
+        "repo": "源代码",
+        "pdf": "PDF 下载"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "语言水平",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "工作与项目",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "技能",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+    dict: {
+        source_code: "源代码",
+        demo: "Demo"
+    },
+    project_type: {
+        "personal": "个人项目"
+    }
+};
